77cdb9bcb4a1ff7a0449b35eddc6b181c9f47bbf
jdiaz-he <jdiaz-he@student.42madrid.com>
2023-03-01 13:27:16 +0100

    agregada parte obligatoria

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdiaz-he <jdiaz-he@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 14:03:33 by jdiaz-he          #+#    #+#             */
+/*   Updated: 2023/03/01 13:11:32 by jdiaz-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_gnl_by_char(int fd);
+char	*ft_gnl_by_buffer(int fd, char **buffer, char *line);
+char	*ft_read_line_buffer(int fd);
+char	*ft_next_line_buffer(char *buffer);
+char	*ft_next_buffer(char *buffer);
+char	*ft_strjoin(char *s1, char *s2, char *free_s1, char *free_s2);
+void	ft_bzero(void *mem, size_t size);
+void	*ft_calloc(size_t nmemb, size_t size);
+size_t	ft_strlen(char *str);
+ssize_t	ft_strchr_index(char *buffer, ssize_t buffer_size, char c);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdiaz-he <jdiaz-he@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/13 14:01:24 by jdiaz-he          #+#    #+#             */
+/*   Updated: 2023/02/28 21:49:48 by jdiaz-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str && str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	ft_bzero(void *mem, size_t size)
+{
+	if (!mem)
+		return ;
+	while (size)
+		((char *) mem)[--size] = '\0';
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*mem;
+
+	mem = malloc(nmemb * size);
+	if (!mem)
+		return (NULL);
+	ft_bzero(mem, nmemb * size);
+	return (mem);
+}
+
+char	*ft_strjoin(char *s1, char *s2, char *free_s1, char *free_s2)
+{
+	char	*str;
+	size_t	len;
+
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	str = ft_calloc(len, sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s1 && *s1)
+		*str++ = *s1++;
+	while (s2 && *s2)
+		*str++ = *s2++;
+	*str = '\0';
+	if (free_s1)
+		free(free_s1);
+	if (free_s2)
+		free(free_s2);
+	return (str - (len - 1));
+}
+
+ssize_t	ft_strchr_index(char *buffer, ssize_t buffer_size, char c)
+{
+	ssize_t	i;
+
+	i = 0;
+	while (buffer && buffer[i] != c && i < buffer_size)
+		i++;
+	if (buffer && buffer[i] == 0)
+		return (-1);
+	return (i);
+}
